4e8117b9fe53a52a15c0645dd6d287f5c3b0b641
Eric <eric.yanez50@qmail.cuny.edu>
2022-02-18 11:07:38 -0500

    commit tree 7ba77ad26f3d0a2228714cb20f0ac04f02a1718b

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+const submitData = function(userName, userEmail){
+   const userDate = {
+       name: `${userName}`,
+       email: `${userEmail}`,
+   };
+   const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(userData),
+    };
+
+    fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((dataObj) => renderUserID(dataObj));
+}
